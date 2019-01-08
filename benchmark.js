4c62c9b4c2c990f2845e0acdbf30773e19aed708
Andras <andras@kinvey.com>
2019-01-08 17:37:19 -0500

    also benchmark lru with 1% and 25% displacement

--- benchmark.js
+++ benchmark.js
@@ -1,7 +1,9 @@
 /**
- * Copyright (C) 2015-2016,2018 Andras Radics
+ * Copyright (C) 2015-2016,2018-2019 Andras Radics
  * Licensed under the Apache License, Version 2.0
  */
+
+// npm install lru-cache node-cache memory-cache
 
 var qtimeit = require('qtimeit');
 var qcache = require('./');
@@ -25,13 +27,18 @@ var x;
 // 39m/s
 
 qtimeit.bench.opsPerTest = nloops * nitems;
-qtimeit.bench.timeGoal = 2;
+qtimeit.bench.timeGoal = 1;
 qtimeit.bench.showRunDetails = false;
 qtimeit.bench.baselineAvg = 2000000;
 qtimeit.bench.visualize = true;
-for (var repeat=0; repeat<5; repeat++) qtimeit.bench({
-    lru_cache: function() {
-        c = lru_cache({ max: 999999999, maxAge: 999999 });
+for (var repeat=0; repeat<5; repeat++) {
+
+console.log("");
+console.log("---------------- Pure cache:");
+qtimeit.bench({
+    'lru-cache': function() {
+        //c = lru_cache({ max: 999999999, maxAge: 999999 });
+        c = lru_cache();
         for (var j=0; j<nloops; j++) {
             for (var i=0; i<nitems; i++) c.set(keys[i], i);
             for (var k=0; k<nreuse; k++) for (var i=0; i<nitems; i++) x = c.get(keys[i]);
@@ -39,7 +46,7 @@ for (var repeat=0; repeat<5; repeat++) qtimeit.bench({
         }
     },
 
-    node_cache: function() {
+    'node-cache': function() {
         c = new node_cache();
         for (var j=0; j<nloops; j++) {
             for (var i=0; i<nitems; i++) c.set(keys[i], i);
@@ -48,22 +55,90 @@ for (var repeat=0; repeat<5; repeat++) qtimeit.bench({
         }
     },
 
-    memory_cache: function() {
+    'memory-cache': function() {
         // global cache, not separate objects ?!
         c = memory_cache;
+        c.set = c.put;
         for (var j=0; j<nloops; j++) {
-            for (var i=0; i<nitems; i++) c.put(keys[i], i);
+            for (var i=0; i<nitems; i++) c.set(keys[i], i);
             for (var k=0; k<nreuse; k++) for (var i=0; i<nitems; i++) x = c.get(keys[i]);
             for (var i=0; i<nitems; i++) c.del(keys[i]);
         }
     },
 
-    qcache: function() {
+    'qcache.TtlCache': function() {
         c = new qcache.TtlCache({ capacity: 999999999, ttl: 999999 });
         for (var j=0; j<nloops; j++) {
             for (var i=0; i<nitems; i++) c.set(keys[i], i);
             for (var k=0; k<nreuse; k++) for (var i=0; i<nitems; i++) x = c.get(keys[i]);
-            for (var i=0; i<nitems; i++) c.delete(keys[i]);
+            for (var i=0; i<nitems; i++) c.del(keys[i]);
         }
     },
-})
+
+    'qcache.LruCache1 v1': function() {
+        c = new qcache.LruCache1({ capacity: 999999999, ttl: 999999 });
+        c.del = c.delete;
+        for (var j=0; j<nloops; j++) {
+            for (var i=0; i<nitems; i++) c.set(keys[i], i);
+            for (var k=0; k<nreuse; k++) for (var i=0; i<nitems; i++) x = c.get(keys[i]);
+            for (var i=0; i<nitems; i++) c.del(keys[i]);
+        }
+    },
+
+    'qcache.LruCache2 v2': function() {
+        c = new qcache.LruCache2({ capacity: 999999999, ttl: 999999 });
+        for (var j=0; j<nloops; j++) {
+            for (var i=0; i<nitems; i++) c.set(keys[i], i);
+            for (var k=0; k<nreuse; k++) for (var i=0; i<nitems; i++) x = c.get(keys[i]);
+            for (var i=0; i<nitems; i++) c.del(keys[i]);
+        }
+    },
+});
+
+console.log("");
+console.log("---------------- LRU cache:");
+qtimeit.bench({
+    'lru-cache 1% lru': function() {
+        c = lru_cache({ max: nitems * 100/101, maxAge: 999999 });
+        testCache(c);
+    },
+
+    'qcache.LruCache v1 1% lru': function() {
+        c = new qcache.LruCache1({ capacity: nitems * 100/101 });
+        c.del = c.delete;
+        testCache(c);
+    },
+
+    'qcache.LruCache v2 1% lru2': function() {
+        c = new qcache.LruCache2({ capacity: nitems * 100/101 });
+        testCache(c);
+    },
+
+    'lru-cache 25% lru': function() {
+        c = lru_cache({ max: nitems * .80, maxAge: 999999 });
+        testCache(c);
+    },
+
+    'qcache.LruCache v1 25% lru': function() {
+        c = new qcache.LruCache1({ capacity: nitems * .80 });
+        c.del = c.delete;
+        testCache(c);
+    },
+
+    'qcache.LruCache v2 25% lru2': function() {
+        c = new qcache.LruCache2({ capacity: nitems * .80 });
+        testCache(c);
+    },
+});
+
+}
+
+// note: the cache benchmarks run faster if each loop can be optimized separately,
+// but the lru benchmarks do not appear sensitive and can be folded.
+function testCache( c ) {
+    for (var j=0; j<nloops; j++) {
+        for (var i=0; i<nitems; i++) c.set(keys[i], i);
+        for (var k=0; k<nreuse; k++) for (var i=0; i<nitems; i++) x = c.get(keys[i]);
+        for (var i=0; i<nitems; i++) c.del(keys[i]);
+    }
+}
